67b0ba861b601e9b0cdfd11794951af11b2d583a
chenw <chenq@zhonghe.info>
2026-06-08 17:37:11 +0800

    动作切换

--- youtube-01/player/player.gd
+++ youtube-01/player/player.gd
@@ -16,10 +16,8 @@ func _physics_process(delta: float) -> void:
 		input_vector = Input.get_vector("run_left" , "run_right" , "run_up" , "run_down")
 		if input_vector != Vector2.ZERO:
 			var direction_vector = Vector2(input_vector.x  , -input_vector.y)
-			last_direction = direction_vector
 			update_blend_positions(direction_vector)
 		if Input.is_action_just_pressed("attack"):
-			animation_tree.set("parameters/StateMachine/AttackState/blend_position" , last_direction)
 			play_back.travel("AttackState")
 	elif state == "AttackState":
 		pass
@@ -31,4 +29,5 @@ func _physics_process(delta: float) -> void:
 func update_blend_positions(direction_vector: Vector2) -> void:
 	animation_tree.set("parameters/StateMachine/MoveState/RunState/blend_position" , direction_vector)
 	animation_tree.set("parameters/StateMachine/MoveState/StandState/blend_position" , direction_vector)
+	animation_tree.set("parameters/StateMachine/AttackState/blend_position" , direction_vector)
 	
